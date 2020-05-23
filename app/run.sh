bcf0c2b304b336a139f502a1662a0de6cf9e5f1e
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-05-22 17:39:02 -0700

    update run.sh

--- app/run.sh
+++ app/run.sh
@@ -2,7 +2,4 @@
 # cd /usr/data/openapispecs
 #!/bin/bash
 
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "Pulling specs at $timestamp"
-
- while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 5; done
+while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 5; done
