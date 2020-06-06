4642a82eac9bcb1be8d5fc6ac263bd3cf90a2982
Sanjai Ganesh <sanjaiga@microsoft.com>
2020-06-06 06:22:16 -0700

    minor update

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,4 @@
 # cd /usr/data/openapispecs
 #!/bin/bash
 
-while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp.";git pull; sleep 5; done
+while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 5; done
