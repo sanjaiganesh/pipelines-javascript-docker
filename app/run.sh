c33abbccaffe5c3dd07667ddfd5be23a39e0b2c8
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-09 23:50:52 -0700

    using absolute path to specrepo

--- app/run.sh
+++ app/run.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 mkdir -p /usr/data/openapispecs/.git
-cp -R ./specrepo/* /usr/data/openapispecs/
-cp -R ./specrepo/.git/* /usr/data/openapispecs/.git/
+cp -R /usr/data/specrepo/* /usr/data/openapispecs/
+cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
 
 while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 5; done
