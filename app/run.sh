ad18e1b9ef3e9f383d8d2ceed0790b6971b3d036
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-09 23:46:50 -0700

    copy files from specrepo into openapispecs

--- app/run.sh
+++ app/run.sh
@@ -1,4 +1,8 @@
 
 #!/bin/bash
 
+mkdir -p /usr/data/openapispecs/.git
+cp -R ./specrepo/* /usr/data/openapispecs/
+cp -R ./specrepo/.git/* /usr/data/openapispecs/.git/
+
 while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 5; done
